18727e34c24798ee742409059960b530b4178fb2
anlaneg <anlaneg@example.com>
2017-11-16 20:38:38 +0800

    add scripts

--- vnf/gen-flow-common.sh
+++ vnf/gen-flow-common.sh
@@ -5,5 +5,8 @@ link 0 up
 link 1 down
 link 1 config 172.16.50.100 24
 link 1 up
+
+p 1 arpadd 0 192.168.50.2  12:03:04:05:07:99
+p 1 arpadd 1 172.16.50.202 12:03:04:05:08:99
 "
 
--- vnf/vCGNAPT-run.sh
+++ vnf/vCGNAPT-run.sh
@@ -9,5 +9,6 @@ tc_file="./acc_swlb_2000_flow.tc"
 ./gen-2000-flow.sh   >> $tc_file
 
 #sudo gdb --args $CGNAPT_PATH/vCGNAPT -p 0x3 -f $CGNAPT_CONFIG_PATH/acc_swlb_2port_2WT.cfg  -s $CGNAPT_CONFIG_PATH/acc_swlb_2port_2WT.tc -l 2
-sudo gdb --args $CGNAPT_PATH/vCGNAPT -p 0x3 -f $cfg_file  -s $tc_file #-l 2
+#sudo gdb --args 
+sudo $CGNAPT_PATH/vCGNAPT -p 0x3 -f $cfg_file  -s $tc_file #-l 2
 
