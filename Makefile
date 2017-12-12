7c040ee7dbd8a06f46f26c0f18a587227c4cf5f3
anlang <anlang@example.com>
2017-12-12 10:51:54 +0800

    finish vnf-only

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
 export BE_ROOT=$(CURDIR)
-
-SUB_MODULE=lib common message plugins rule be_la tool vnf
-#MODULE_BEFORE=
-#MODULE_AFTER=
-#CLEAN_AFTER=
-
+SUB_MODULE= vnf
 include $(BE_ROOT)/mk/rule.mk
---
+++ vnf/cgnat-acc/demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int cgnat_accelerate(void*id, uint8_t srcmac[6],uint8_t dstmac[6],uint32_t src_ip,
+                uint16_t src_port, uint8_t protocol, uint32_t xlate_src_ip,
+                uint16_t xlate_src_port, uint8_t xlate_srcmac[6],
+                uint8_t xlate_dstmac[6])
+{
+	return 0;
+}
