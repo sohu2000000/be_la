d4e76b2ab46d4bf1d513e2ff4cf9a4d6b2b48b73
anlang <anlang@example.com>
2017-12-12 13:05:38 +0800

    gen export.vnf-only.tar.gz

--- export.sh
+++ export.sh
@@ -1,2 +1,15 @@
 #! /bin/bash
-git checkout-index -a -f --prefix=`pwd`/../export/
+tmp_dir=`pwd`/export-dir/
+
+#prepare workspace
+rm -rf $tmp_dir
+mkdir $tmp_dir
+
+#export
+git checkout-index -a -f --prefix=$tmp_dir
+
+#create tar
+tar -zcvf export.vnf-only.tar.gz $tmp_dir
+
+#clean up
+rm -rf $tmp_dir
